aea6b789714f21d330a4603f2374ca0c786a8ffc
Libin Salal <libin.salal@dominos.com>
2016-05-02 22:44:27 -0400

    Handled the orientation change

--- app/src/main/java/com/libin/loaderactivitytest/LoaderActivity.java
+++ app/src/main/java/com/libin/loaderactivitytest/LoaderActivity.java
@@ -26,9 +26,13 @@ public abstract class LoaderActivity extends AppCompatActivity implements Loader
      */
     private static final String KEY_LOADER_BUNDLE = "LOADER_BUNDLE";
     /**
+     * Key for loader request code
+     */
+    private static final String KEY_LOADER_REQUEST_CODE = "LOADER_REQUEST_CODE";
+    /**
      * Generate unique integer value for loader id.
      */
-    private static AtomicInteger mUniqueLoaderId = new AtomicInteger(1);
+    private static AtomicInteger mUniqueLoaderId = new AtomicInteger(0);
     private int mLoaderId;
     private int mLoaderRequestCode = -1;
     private Bundle mLoaderBundle;
@@ -38,8 +42,9 @@ public abstract class LoaderActivity extends AppCompatActivity implements Loader
         super.onCreate(savedInstanceState);
 
         if(savedInstanceState != null) {
-            mLoaderId = savedInstanceState.getInt(KEY_LOADER_ID , createUniqueLoaderId());
+            mLoaderId = savedInstanceState.getInt(KEY_LOADER_ID);
             mLoaderBundle = savedInstanceState.getBundle(KEY_LOADER_BUNDLE);
+            mLoaderRequestCode = savedInstanceState.getInt(KEY_LOADER_REQUEST_CODE);
         }else {
             mLoaderId = createUniqueLoaderId();
         }
@@ -52,10 +57,11 @@ public abstract class LoaderActivity extends AppCompatActivity implements Loader
         super.onSaveInstanceState(outState);
         outState.putInt(KEY_LOADER_ID , mLoaderId);
         outState.putBundle(KEY_LOADER_BUNDLE , mLoaderBundle);
+        outState.putInt(KEY_LOADER_REQUEST_CODE , mLoaderRequestCode);
     }
 
     @Override
-    public Loader onCreateLoader(int id, final Bundle args) {
+    public Loader onCreateLoader(final int id, final Bundle args) {
         return new AsyncTaskLoader<Object>(this) {
             @Override
             public Object loadInBackground() {
@@ -66,29 +72,27 @@ public abstract class LoaderActivity extends AppCompatActivity implements Loader
                 // load new data
                 if(mLoaderRequestCode >= 0) {
                     forceLoad();
-                    Log.d(TAG, "onStartLoading  called and forced to load data");
+                    Log.d(TAG, "onStartLoading called for id = " + id + " and forced to load data");
                 }else {
-                    Log.d(TAG, "onStartLoading  called but the loader is not requested");
+                    Log.d(TAG, "onStartLoading called for id = " + id + " but the loader is not requested");
                 }
             }
 
             @Override
             protected void onStopLoading() {
-                Log.d(TAG, "onStopLoading  called");
-                cancelLoad();
-                clearLoader();
+                Log.d(TAG, "onStopLoading called for loader id = "  + id);
+//                cancelLoad();
             }
 
             @Override
             public void onCanceled(Object data) {
-                Log.d(TAG, "onCanceled  called");
+                Log.d(TAG, "onCanceled  called for loader id = " + id);
                 clearLoader();
             }
 
             @Override
             protected void onReset() {
-                Log.d(TAG, "onReset  called");
-                clearLoader();
+                Log.d(TAG, "onReset  called for loader id = " + id);
             }
         };
     }
@@ -97,13 +101,12 @@ public abstract class LoaderActivity extends AppCompatActivity implements Loader
     public void onLoadFinished(Loader loader, Object data) {
         // deliver result
         onLoaderResult(mLoaderRequestCode , data);
-        Log.d(TAG, "Loader of id = " + loader.getId() +  "delivered the result");
+        Log.d(TAG, "Loader of id = " + loader.getId() +  " delivered the result");
     }
 
     @Override
     public void onLoaderReset(Loader loader) {
-        clearLoader();
-        Log.d(TAG, "Loader of id = " + loader.getId() +  "is reset");
+        Log.d(TAG, "Loader of id = " + loader.getId() +  " is reset");
     }
 
     private void clearLoader() {
@@ -111,7 +114,7 @@ public abstract class LoaderActivity extends AppCompatActivity implements Loader
     }
 
     protected void startLoaderForResult(int requestCode, Bundle bundle) {
-        Log.d(TAG, "Restarting loader for the request code = " + requestCode);
+        Log.d(TAG, "Restarting loader for request code = " + requestCode);
         mLoaderRequestCode = requestCode;
         getSupportLoaderManager().restartLoader(mLoaderId , bundle , this);
     }
