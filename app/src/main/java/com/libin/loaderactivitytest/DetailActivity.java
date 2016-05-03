7e6db48ea540470c79cbc1ccf4a609c66c71b93c
Libin Salal <libin.salal@dominos.com>
2016-05-02 22:44:59 -0400

    Added more test activity

---
+++ app/src/main/java/com/libin/loaderactivitytest/DetailActivity.java
@@ -0,0 +1,40 @@
+package com.libin.loaderactivitytest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+/**
+ * @author Libin
+ */
+public class DetailActivity extends LoaderActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        showResult("Loading..");
+        startLoaderForResult(0 , null);
+    }
+
+    @Override
+    protected Object loadDataInBackground(Bundle bundle) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Loaded detail from background";
+    }
+
+    @Override
+    protected void onLoaderResult(int requestCode, Object data) {
+        showResult(data.toString());
+    }
+
+    private void showResult(String result){
+        // Toast.makeText(this , data.toString() , Toast.LENGTH_LONG).show();
+        TextView textView = (TextView) findViewById(R.id.detail_view);
+        textView.setText(result);
+    }
+}
---
+++ app/src/main/java/com/libin/loaderactivitytest/ListActivity.java
@@ -0,0 +1,53 @@
+package com.libin.loaderactivitytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ListActivity extends LoaderActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.testLoader).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showResult("Loading..");
+                startLoaderForResult(0 , null);
+            }
+        });
+
+        findViewById(R.id.testDetail).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ListActivity.this , DetailActivity.class));
+            }
+        });
+
+    }
+
+    @Override
+    protected Object loadDataInBackground(Bundle bundle) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Loaded list items from background";
+    }
+
+    @Override
+    protected void onLoaderResult(int requestCode, Object data) {
+       showResult(data.toString());
+    }
+
+
+    private void showResult(String result){
+        // Toast.makeText(this , data.toString() , Toast.LENGTH_LONG).show();
+        TextView textView = (TextView) findViewById(R.id.listTextView);
+        textView.setText(result);
+    }
+
+}
